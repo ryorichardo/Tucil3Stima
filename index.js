074e4256333a4c80087808762362f2eca7d29889
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2021-04-03 18:53:28 +0700

    Create: Front-end map

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+var path = require('path');
 const app = express();
 const port = 3000;
+const mapboxgl = require('mapbox-gl');
+
+app.use( express.static( "public" ) );
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index');
+});
+
+app.get('/test', (req, res) => {
+    res.render('test');
 });
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 });
+
