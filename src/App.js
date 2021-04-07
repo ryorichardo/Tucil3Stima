f5deee7a599ab677bcf39196be10c0f5a385d4f4
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2021-04-07 07:15:29 +0700

    initial

---
+++ src/App.js
@@ -0,0 +1,215 @@
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup, Source, Layer } from "react-map-gl";
+import aStar from "./tes.js";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  const [selectedPark, setSelectedPark] = useState(null);
+  const [selectedStartEnd, setSelectedStartEnd] = useState([]);
+  const [data, setData] = useState({});
+  const [coor, setCoor] = useState([]);
+
+  const [viewport, setViewport] = useState({});
+
+  const SIZE = 20;
+  const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7`;
+
+  let polylineGeoJSON = {
+    'type': 'Feature',
+    'properties': {},
+    'geometry': {
+        'type': 'LineString',
+        'coordinates': [] // [107.60845619489565, -6.8938593825294925], [107.61043792489653, -6.893194719054599]
+    }
+  }
+  
+  const handleClick = () => {
+    console.log(polylineGeoJSON);
+    setSelectedStartEnd([]);
+    setSelectedPark(null);
+  }
+
+  const readFileOnUpload = (uploadedFile) => {
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      setData(JSON.parse(fileReader.result));
+
+      setSelectedStartEnd([]);
+      setSelectedPark(null);
+      setViewport({
+        latitude: JSON.parse(fileReader.result).nodes[0].coordinate[1],
+        longitude: JSON.parse(fileReader.result).nodes[0].coordinate[0],
+        minWidth: "100vw",
+        minHeight: "100vh",
+        zoom: 15
+      });
+
+      let coorTemp = [];
+      JSON.parse(fileReader.result).nodes.forEach(element => {
+          coorTemp.push(element.coordinate);
+      })
+      setCoor(coorTemp);
+    };
+    if (uploadedFile !== undefined) {
+       fileReader.readAsText(uploadedFile);
+    }
+ }
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <div>
+        <Form style={{
+          margin: 'auto',
+          width: '50%',
+          border: '3px solid green',
+          padding: '10px',
+        }}>
+          <Form.Group style={{
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '50%',
+            padding: '10px',
+          }}>
+            <Form.File id="fileForm">
+              <Form.File.Label style={{
+                display: 'block',
+                textAlign: 'center',
+                lineHeight: '150%',
+              }}>Silakan upload file JSON sebagai peta</Form.File.Label>
+              <Form.File.Input
+                style={{
+                  'text-align': 'center',
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                }}
+                onChange={(e)=>readFileOnUpload(e.target.files[0])} />
+            </Form.File>
+          </Form.Group>
+        </Form>
+
+      {(JSON.stringify(data) !== JSON.stringify({})) ? (
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        <Button
+          className="resetButton"
+          style={{ 
+            marginTop: '10px',
+            marginLeft: '90%',
+            height: '50px', 
+            width : '150px'
+          }}
+          onClick={handleClick}
+        >
+          Reset Nodes
+        </Button>
+
+        {data.nodes.map(node => (
+          <Marker
+            key={node.id}
+            latitude={node.coordinate[1]}
+            longitude={node.coordinate[0]}
+          >
+            <svg
+              height={SIZE}
+              viewBox="0 0 24 24"
+              style={{
+                cursor: 'pointer',
+                fill: '#008000',
+                stroke: 'none',
+                transform: `translate(${-SIZE / 2}px,${-SIZE}px)`
+              }}
+              onClick={e => {
+                e.preventDefault();
+                setSelectedPark(node);
+
+                console.log(selectedStartEnd);
+                if (selectedStartEnd.length === 0) {
+                  setSelectedStartEnd([node]);
+                  console.log(selectedStartEnd);
+                }
+                else if (selectedStartEnd.length === 1) {
+                  setSelectedStartEnd([selectedStartEnd[0], node]);
+                  let matrix = data["adjacency matrix"];
+                  let coor = []
+                  data.nodes.forEach(element => {
+                      coor.push(element.coordinate);
+                  })    
+                  let e = aStar(selectedStartEnd[0].id, node.id, matrix, coor);
+                  console.log(polylineGeoJSON);
+                }
+              }}
+            >
+              <path d={ICON} />
+            </svg>
+          </Marker>
+        ))}
+
+        {selectedPark ? (
+          <Popup
+            latitude={selectedPark.coordinate[1]}
+            longitude={selectedPark.coordinate[0]}
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPark.name}</h2>
+            </div>
+          </Popup>
+        ) : null}
+
+        {(selectedStartEnd.length === 2) ? (
+        <Source id='polylineLayer' type='geojson' data={{
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+              'type': 'LineString',
+              'coordinates': aStar(selectedStartEnd[0].id, selectedStartEnd[1].id, data["adjacency matrix"], coor)
+          }
+        }} >
+          {(selectedStartEnd.length === 2) ? (
+              <Layer
+                id='lineLayer'
+                type='line'
+                source='my-data'
+                layout={{
+                'line-join': 'round',
+                'line-cap': 'round',
+                }}
+                paint={{
+                'line-color': 'rgba(3, 170, 238, 0.5)',
+                  'line-width': 5,
+                }}
+              />
+            ) : null}
+          </Source>
+          ) : <Source id='polylineLayer' type='geojson' data={polylineGeoJSON}>
+            </Source>}
+      
+      </ReactMapGL>
+      ) : null}
+    </div>
+  );
+}
---
+++ src/tes.js
@@ -0,0 +1,162 @@
+class Element { 
+    constructor(index, list, wV, w) {
+        this.idx = index;       // index node
+        this.visited = list;    // node yang sudah dikunjungi
+        this.weightVisited = wV;// bobot jarak yg sudah dilewati (sama seperti g(n))
+        this.weight = w;        // bobot node (sama seperti g(n) + h(n))
+    }
+  
+    setIdx(index) {
+        this.idx = index;
+    }
+  
+    getIdx() {
+        return this.idx;
+    }
+  
+    addNode(index) {
+        this.visited.push(index);
+    }
+  
+    getVisited() {
+        return this.visited;
+    }
+  
+    getWeightVisited() {
+        return this.weightVisited;
+    }
+  
+    getWeight() {
+        return this.weight;
+    }
+  }
+class PriorityQueue {
+    constructor() {
+        this.list = [];         // isi queue
+        this.hasVisited = [];   // isi node2 yang sudah dikunjungi (unik)
+    }
+    
+    isEmpty() {
+        return (this.list.length === 0); 
+    }
+    
+    enqueue(element) {
+        // Kosong
+        if (this.isEmpty()) {
+            this.list.push(element);
+            this.hasVisited.push(element.getIdx());
+        }
+
+        // Element belum ada di hasVisited; element baru dikunjungi
+        // const id = (el) => el === element.getIdx();
+        if (!this.hasVisited.includes(element.getIdx())){
+            let i = 0;
+            let pushed = false;
+            while (i < this.list.length && !pushed) {
+              if (this.list[i].getWeight() > element.getWeight()) {
+                this.list.splice(i, 0, element);
+                pushed = true;
+              }
+              i++;
+            }
+            // Element baru paling besar; i mencapai max
+            if (!pushed) {
+              this.list.push(element);
+            }
+            
+            // Tambah hasVisited
+            this.hasVisited.push(element.getIdx());
+        }
+      
+        
+    }
+  
+    dequeue() {
+        if (!this.isEmpty()) {
+            return this.list.shift();
+        }
+    }
+  
+    peek() {
+        if (!this.isEmpty()) {
+            return this.list[0];
+        }
+    }
+}
+
+
+function haversineDistance(long1, lat1, long2, lat2) {
+    // Diambil dari https://www.movable-type.co.uk/scripts/latlong.html
+    const r = 6371000 // radius bumi 6317000 m
+    const sigma1 = lat1 * Math.PI/180;
+    const sigma2 = lat2 * Math.PI/180;
+    const deltasigma = (lat2 - lat1) * Math.PI/180;
+    const deltalambda = (long2 - long1) * Math.PI/180
+
+    const a = Math.sin(deltasigma/2) * Math.sin(deltasigma/2) + Math.cos(sigma1) * Math.cos(sigma2) * Math.sin(deltalambda/2) * Math.sin(deltalambda/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return r * c;
+}
+
+
+// Algoritma A*
+
+// Inisialisasi elemen pertama dan queue
+export default function aStar(idxsrc, idxdes, matrix, coor) {
+    // Kamus
+    // idxsrc   : indeks start node
+    // idxdes   : indeks destination node
+    // matrix   : adjacency matrix
+    // long     : list of longitudes
+    // lat      : list of lattitudes
+
+    let el = new Element(idxsrc, [], 0, 0);
+    let queue = new PriorityQueue();
+    queue.enqueue(el);
+    // console.log(queue);
+    // queue.dequeue();
+    // console.log(queue);
+
+    // Pengulangan sampai top of queue adalah node tujuan atau queue kosong
+    while (el.getIdx() !== idxdes && !queue.isEmpty()) {
+        el = queue.dequeue();
+        el.addNode(el.getIdx());
+    
+        // Mencari adjacency di matrix
+        for (let kolom = 0; kolom < matrix[el.getIdx()].length; kolom++) {
+            if (matrix[el.getIdx()][kolom] === 1) {
+                let distanceElNextEl = haversineDistance(coor[el.getIdx()][0], coor[el.getIdx()][1], coor[kolom][0], coor[kolom][1]); // hitung jarak pindah elemen
+                let heuristicDistance = haversineDistance(coor[idxdes][0], coor[idxdes][1], coor[kolom][0], coor[kolom][1]); // hitung jarak heuristik nextEl ke tujuan
+                let newWeightVisited = el.getWeightVisited() + distanceElNextEl; // jarak baru (g(n))
+                let newWeight = newWeightVisited + heuristicDistance; // bobot baru (g(n) + h(n))
+                // let nextEl = new Element(kolom, el.gethasVisited(), newWeightVisited, newWeight) // bikin next element
+                // nextEl.addNode(el.getIdx()); // nambah node yg udah dikunjungi
+                queue.enqueue(new Element(kolom, el.getVisited(), newWeightVisited, newWeight)); // enqueue
+            }
+        }
+      
+        // Next element
+        if (!queue.isEmpty()) {
+            el = queue.peek();
+        }
+    }
+  
+    // Isi el.visited dengan diri sendiri
+    el.addNode(el.getIdx());
+
+    console.log(queue);
+  
+    // Kondisi ketemu
+    if (el.getIdx() === idxdes) {
+        let res = [];
+        el.visited.forEach(element => {
+            res.push(coor[element]);
+        })
+        return res;
+    }
+  
+    // Kondisi gaketemu array kosong
+    else {
+        return [];
+    }
+}
